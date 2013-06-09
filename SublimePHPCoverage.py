3d0654451cca9d878ce0768f25d32cef8d180a7f
Brad Feehan <git@bradfeehan.com>
2013-06-09 19:22:56 +1000

    Split into separate files

--- SublimePHPCoverage.py
+++ SublimePHPCoverage.py
@@ -1,9 +1,12 @@
-import sublime, sublime_plugin
+import os
+import sys
+
+# Add current directory to Python's import path, to import php_coverage
+sys.path.append(os.path.dirname(__file__))
+import php_coverage.debug
+
+debug_message = php_coverage.debug.debug_message
 
 
 def plugin_loaded():
     debug_message("entering plugin_loaded()")
-
-
-def debug_message(message):
-    print("[PHPCoverage] " + str(message))
---
+++ php_coverage/debug.py
@@ -0,0 +1,8 @@
+
+def debug_message(message):
+    """
+    Prints a debug message to the Sublime console
+
+    TODO: Add a configurable plugin setting to disable console messages
+    """
+    print("[PHPCoverage] " + str(message))
