6bfcf89fedaa46eb2612efd878e9b41c48a9fdc7
Brad Feehan <git@bradfeehan.com>
2013-06-10 17:19:52 +1000

    Add FileWatcher class

---
+++ php_coverage/watcher.py
@@ -0,0 +1,146 @@
+import hashlib
+import os
+
+from php_coverage.debug import debug_message
+from php_coverage.threading import PollingThread
+
+# Chunk size used to read file in
+CHUNK_SIZE = 1024 * 1024
+
+
+class FileWatcher(PollingThread):
+
+    """
+    Watches a file for changes, calling a callback every time the file
+    is modified.
+    """
+
+    CREATED = 'created'      # didn't exist before, does now
+    DELETED = 'deleted'      # existed before, doesn't now
+    MODIFIED = 'modified'    # mtime changed, different content
+    UNCHANGED = 'unchanged'  # mtime changed, same content
+
+    def __init__(self, filename):
+        super(FileWatcher, self).__init__()
+        self.filename = filename
+        self.callbacks = {
+            self.CREATED: {},
+            self.DELETED: {},
+            self.MODIFIED: {},
+            self.UNCHANGED: {},
+        }
+
+    def add_callback(self, events, id, callback):
+        """
+        Adds a new callback function for particular events. When one of
+        the events in "events" occurs, callback will be called without
+        any arguments.
+        """
+        # make events a list if only one passed in
+        if type(events) is not list:
+            events = [events]
+
+        # add callback to each event in events
+        for event in events:
+            if not id in self.callbacks[event]:
+                self.callbacks[event][id] = callback
+
+    def has_callbacks(self):
+        """
+        Determines whether or not this FileWatcher has any callbacks.
+        Returns True if at least one callback exists for at least one
+        event, otherwise False.
+        """
+        for callbacks in self.callbacks.values():
+            for callback in callbacks.values():
+                return True
+
+        return False
+
+    def remove_callback(self, id):
+        """
+        Removes an existing callback for particular events. The "id"
+        parameter identifies the callback by the same "id" parameter
+        passed to add_callback() initally.
+        """
+        # remove callback from each event, if it exists
+        for event in self.callbacks:
+            if id in self.callbacks[event]:
+                del self.callbacks[event][id]
+
+    def dispatch(self, event):
+        """
+        Dispatches an event, calling all relevant callbacks.
+        """
+        callbacks = self.callbacks[event]
+
+        debug_message("[FileWatcher] %s '%s'" % (event, self.filename))
+        debug_message("[FileWatcher] %d callbacks" % len(callbacks))
+
+        for callback in callbacks.values():
+            debug_message("[FileWatcher] Calling %s" % repr(callback))
+            callback()
+
+    def hash(self):
+        """
+        Gets the hash of the file referred to by self.filename. Returns
+        None if the file doesn't exist.
+        """
+        if not os.path.exists(self.filename):
+            return None
+
+        sha1 = hashlib.sha1()
+        with open(self.filename, 'rb') as f:
+            while True:
+                data = f.read(CHUNK_SIZE)
+                if not data:
+                    break
+                sha1.update(data)
+
+        return sha1.digest()
+
+    def mtime(self):
+        """
+        Gets the last modified time of the file referred to by
+        self.filename. Returns None if the file doesn't exist.
+        """
+        if not os.path.exists(self.filename):
+            return None
+
+        return os.path.getmtime(self.filename)
+
+    def start(self):
+        self.last_mtime = self.mtime()
+        self.last_hash = self.hash()
+        if self.last_mtime:
+            debug_message("[FileWatcher] exists: %s" % self.filename)
+        else:
+            debug_message("[FileWatcher] doesn't exist: %s" % self.filename)
+        super(FileWatcher, self).start()
+
+    def poll(self):
+        """
+        Checks the modified time of the file and dispatches events
+        representing any changes to it.
+        """
+        new_mtime = self.mtime()
+
+        # if unchanged, do nothing
+        if self.last_mtime == new_mtime:
+            return
+
+        new_hash = self.hash()
+
+        # otherwise dispatch the corresponding event
+        if not self.last_mtime:
+            self.dispatch(self.CREATED)
+        elif not new_mtime:
+            self.dispatch(self.DELETED)
+        elif self.last_hash != new_hash:
+            self.dispatch(self.MODIFIED)
+        else:
+            self.dispatch(self.UNCHANGED)
+
+        # save new mtime for next poll
+        self.last_mtime = new_mtime
+        self.last_hash = new_hash
---
+++ tests/testwatcher.py
@@ -0,0 +1,75 @@
+import os
+import threading
+import unittest
+
+from php_coverage.watcher import FileWatcher
+from unittest.mock import Mock, MagicMock
+
+CREATED = FileWatcher.CREATED
+DELETED = FileWatcher.DELETED
+MODIFIED = FileWatcher.MODIFIED
+UNCHANGED = FileWatcher.UNCHANGED
+
+
+class TestFileWatcher(unittest.TestCase):
+
+    def setUp(self):
+        dir = os.path.join(os.path.dirname(__file__), 'watcher')
+        if not os.path.exists(dir):
+            os.mkdir(dir)
+        self.file = os.path.join(dir, 'test.txt')
+        self.watcher = FileWatcher(self.file)
+        self.detected = threading.Event()
+        self.delete()
+
+    def tearDown(self):
+        if self.watcher.is_alive():
+            self.watcher.stop(1)
+
+        assert(not self.watcher.is_alive())
+
+    def test_created(self):
+        self.watcher.add_callback(CREATED, 1, lambda: self.detected.set())
+        self.watcher.start()
+        self.create('created')
+        self.assertTrue(self.detected.wait(1))
+
+    def test_deleted(self):
+        self.watcher.add_callback(DELETED, 1, lambda: self.detected.set())
+        self.create('deleted')
+        self.watcher.start()
+        self.delete()
+        self.assertTrue(self.detected.wait(1))
+
+    def test_modified(self):
+        self.watcher.add_callback(MODIFIED, 1, lambda: self.detected.set())
+        self.create('modified1')
+        self.watcher.start()
+        self.modify('modified2')
+        self.assertTrue(self.detected.wait(1))
+
+    def test_unchanged(self):
+        self.watcher.add_callback(UNCHANGED, 1, lambda: self.detected.set())
+        self.create('unchanged')
+        self.watcher.start()
+        self.modify('unchanged')
+        self.assertTrue(self.detected.wait(1))
+
+    def create(self, content, mode='x'):
+        if not os.path.exists(os.path.dirname(self.file)):
+            os.mkdir(os.path.dirname(self.file))
+
+        with open(self.file, mode) as file:
+            file.write(content)
+
+    def modify(self, content):
+        self.create(content, 'w')
+
+        # bump mtime 10s into the future
+        new_atime = os.path.getatime(self.file) + 10
+        new_mtime = os.path.getmtime(self.file) + 10
+        os.utime(self.file, (new_atime, new_mtime))
+
+    def delete(self):
+        if os.path.exists(self.file):
+            os.remove(self.file)
